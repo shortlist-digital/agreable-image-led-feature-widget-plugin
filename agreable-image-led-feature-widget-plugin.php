81822eb99b5f1ae71a4ace51d2373d655247bd84
Leo <leonoragilmour@sky.com>
2017-07-26 17:12:26 +0100

    init

---
+++ agreable-image-led-feature-widget-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: Agreable Image Led Feature Widget Plugin
+* Plugin URI: http://github.com/shortlist-digital/agreable-image-led-feature-widget-plugin
+* Description: A plugin for the image led feature widget
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+class AgreableImageLedFeatureWidgetPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new AgreableImageLedFeatureWidgetPlugin();
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Agreable Image Led Feature Widget Plugin
+A plugin for the image led feature widget
